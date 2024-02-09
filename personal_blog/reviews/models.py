e2d9159f07ccc4badb242129351c4de7ee555972
Igorswrk <Igorsantiago.work@gmail.com>
2024-02-09 17:24:30 -0300

    [PERSONAL_BLOG] static files and media config done

--- personal_blog/reviews/models.py
+++ personal_blog/reviews/models.py
@@ -10,8 +10,12 @@ class Movie(models.Model):
     movie_name = models.CharField(max_length=100)
     movie_year = models.IntegerField()
     movie_director = models.CharField(max_length=100)
+<<<<<<< HEAD
     print
     movie_cover_image = models.ImageField(upload_to='movies/images/movies_covers/')
+=======
+    movie_cover_image = models.ImageField(upload_to=f"personal_blog\\reviews\\static\\movies\\{movie_name}\\")
+>>>>>>> f51f039bba476a3720ab55a3f9e391e3ac23a723
     movie_rating = models.DecimalField(max_digits=3, decimal_places=1)
 
     def __str__(self):
--- personal_blog/reviews/urls.py
+++ personal_blog/reviews/urls.py
@@ -5,10 +5,8 @@ from django.conf.urls.static import static
 from .views import MovieListView, MovieDetailView
 
 urlpatterns = [
-    path("movies/", MovieListView.as_view(), name='movie_list'),
-    path("movies/<int:pk>", MovieDetailView.as_view(), name='movie_detail'),
-    # path("movies/add/", MovieCreateView.as_view(), name='movie_add'),      
-] 
+    path("movies/", MovieListView.as_view(), name="movie_list"),       
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
